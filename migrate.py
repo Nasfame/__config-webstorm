a9b2774a9920938b1a75901b39c74af2429587ba
Hiro <goferHiroHiro@gmail.com>
2023-02-17 05:00:04 +0530

    automated by baymax

---
+++ migrate.py
@@ -0,0 +1,27 @@
+import os
+
+import shutil
+
+import platform
+
+sources = ["keymaps","migrate.py","_windows","git2"]
+destinations = ["goland","webstorm","rubymine","pycharm","datagrip","dataspell"]
+
+pwd = os.getcwd()
+
+for dest in destinations:
+  if dest in pwd:
+    continue
+  dest_path = f"../{dest}/"
+  print(dest)
+  for src in sources:
+    destination = dest_path+src
+    if os.path.isfile(src):
+      if os.path.exists(destination):   os.remove(destination)
+      shutil.copy2(src,destination)
+    elif os.path.isdir(src):
+      if os.path.exists(destination): shutil.rmtree(destination)
+      shutil.copytree(src,destination,dirs_exist_ok=True)
+
+print()
+print("bye")
